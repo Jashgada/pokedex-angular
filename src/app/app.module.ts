cda7316fa912c9272705718c752c27fc2384cf8f
jashgada <jashgada93@gmail.com>
2020-09-22 15:39:41 -0400

    added search and filter capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { PokeInfoComponent } from './poke-info/poke-info.component';
 
@@ -10,7 +10,8 @@ import { PokeInfoComponent } from './poke-info/poke-info.component';
     PokeInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/poke-info/poke-info.component.ts
+++ src/app/poke-info/poke-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, from } from 'rxjs';
 
 @Component({
   selector: 'app-poke-info',
@@ -23,9 +24,31 @@ export class PokeInfoComponent implements OnInit {
                    'Mewtwo',
                    'Mewtwo',
                    'Mewtwo'];
+    filteredPokeList: any[] = [];
+
+    _listFilter : string = "";
+
+    get listFilter(): string{
+
+      return this._listFilter;
+    }
+
+    set listFilter(value:string){
+      this._listFilter = value;
+      this.filteredPokeList = this.listFilter?this.performFilter(this.listFilter) : this.pokeList;
+    }
+
+    performFilter(filterBy: string): any[]
+    {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.pokeList.filter((product:string)=>
+        product.toLocaleLowerCase().indexOf(filterBy)!==-1);
+    }
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredPokeList =this.pokeList;
+    console.log(this.filteredPokeList[1]);
   }
 
 }
